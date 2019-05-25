e5c604ea09ad063c6a24000d5ee9e1539526e4ec
dursun gündoğan <>
2019-05-25 04:06:52 +0100

    init

---
+++ src/main/java/co/uk/lambda/AccountManagementApplication.java
@@ -0,0 +1,13 @@
+package co.uk.lambda;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class AccountManagementApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AccountManagementApplication.class, args);
+	}
+}
